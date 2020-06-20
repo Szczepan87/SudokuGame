19e81eeccb3726b9396c8387e33ceb6486f13f84
Szczepan <l.szczepan@wp.pl>
2020-06-20 17:22:52 +0200

    SudokuVerifier refactor class with tests

--- app/src/main/java/com/example/sudokuapplication/util/SudokuVerifier.kt
+++ app/src/main/java/com/example/sudokuapplication/util/SudokuVerifier.kt
@@ -1,48 +1,53 @@
 package com.example.sudokuapplication.util
-//
-//import com.example.sudokuapplication.model.Cell
-//
-//class SudokuVerifier {
-//
-//    fun isSudokuSolved(board: MutableList<Cell>): Boolean {
-//        return isSquareCorrect(board) && isRowCorrect(board) && isColumnCorrect(board)
-//    }
-//
-//    private fun isSquareCorrect(board: MutableList<Cell>): Boolean {
-//        val sublist = mutableListOf<Int>()
-//        for (col in 0 until SQUARE_SIZE step SQUARE_SIZE) {
-//            for (row in 0 until BOARD_SIZE) {
-//                sublist.addAll(board.board[row].subList(col, col + SQUARE_SIZE))
-//                if ((row + 1) % SQUARE_SIZE == 0) {
-//                    if (!isInputCorrect(sublist)) return false else sublist.clear()
-//                }
-//            }
-//        }
-//        return true
-//    }
-//
-//    private fun isRowCorrect(board: MutableList<Cell>): Boolean {
-//        val sublist = mutableListOf<Int>()
-//        for (i in 0 until BOARD_SIZE) {
-//            if (!isInputCorrect(board.board[i])) return false
-//            else sublist.clear()
-//        }
-//        return true
-//    }
-//
-//    private fun isColumnCorrect(board: MutableList<Cell>): Boolean {
-//        val sublist = mutableListOf<Int>()
-//        for (i in 0 until BOARD_SIZE) {
-//            board.board.forEach { sublist.add(it[i]) }
-//            if (!isInputCorrect(sublist)) return false
-//            else sublist.clear()
-//        }
-//        return true
-//    }
-//
-//    private fun isInputCorrect(list: List<Cell>): Boolean {
-//        val set = mutableSetOf<Cell>()
-//        set.addAll(list)
-//        return set.size == list.size
-//    }
-//}
+
+import com.example.sudokuapplication.model.Cell
+
+class SudokuVerifier {
+
+    fun isSudokuSolved(board: MutableList<Cell>): Boolean {
+        return isSquareCorrect(board) && isRowCorrect(board) && isColumnCorrect(board)
+    }
+
+    private fun isSquareCorrect(board: MutableList<Cell>): Boolean {
+        val sublist = mutableListOf<Int>()
+        for (column in 0 until BOARD_SIZE step SQUARE_SIZE){
+            for (row in 0 until BOARD_SIZE) {
+                sublist.add(board.getCell(row, column).value)
+                sublist.add(board.getCell(row, column + 1).value)
+                sublist.add(board.getCell(row, column + 2).value)
+                if (row + 1 % 3 == 0){
+                    if (!isInputCorrect(sublist)) return false else sublist.clear()
+                }
+            }
+        }
+        return true
+    }
+
+    private fun isRowCorrect(board: MutableList<Cell>): Boolean {
+        val sublist = mutableListOf<Int>()
+        for (row in 0 until BOARD_SIZE) {
+            for (col in 0 until BOARD_SIZE) {
+                sublist.add(board.getCell(row, col).value)
+            }
+            if (!isInputCorrect(sublist)) return false else sublist.clear()
+        }
+        return true
+    }
+
+    private fun isColumnCorrect(board: MutableList<Cell>): Boolean {
+        val sublist = mutableListOf<Int>()
+        for (col in 0 until BOARD_SIZE) {
+            for (row in 0 until BOARD_SIZE) {
+                sublist.add(board.getCell(row, col).value)
+            }
+            if (!isInputCorrect(sublist)) return false else sublist.clear()
+        }
+        return true
+    }
+
+    private fun isInputCorrect(list: List<Int>): Boolean {
+        val set = mutableSetOf<Int>()
+        set.addAll(list)
+        return set.size == list.size
+    }
+}
--- app/src/test/java/com/example/sudokuapplication/util/SudokuVerifierTest.kt
+++ app/src/test/java/com/example/sudokuapplication/util/SudokuVerifierTest.kt
@@ -1,87 +1,87 @@
 package com.example.sudokuapplication.util
-//
-//import com.example.sudokuapplication.model.Board
-//import org.junit.Assert.assertFalse
-//import org.junit.Assert.assertTrue
-//import org.junit.Test
-//
-//class SudokuVerifierTest {
-//
-//    private val sudokuVerifier = SudokuVerifier()
-//
-//    private val goodRowsWrongSudoku = Board(
-//        mutableListOf(
-//            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-//        )
-//    )
-//
-//    private val goodColumnsWrongSudoku = Board(
-//        mutableListOf(
-//            mutableListOf(1, 1, 1, 1, 1, 1, 1, 1, 1),
-//            mutableListOf(2, 2, 2, 2, 2, 2, 2, 2, 2),
-//            mutableListOf(3, 3, 3, 3, 3, 3, 3, 3, 3),
-//            mutableListOf(4, 4, 4, 4, 4, 4, 4, 4, 4),
-//            mutableListOf(5, 5, 5, 5, 5, 5, 5, 5, 5),
-//            mutableListOf(6, 6, 6, 6, 6, 6, 6, 6, 6),
-//            mutableListOf(7, 7, 7, 7, 7, 7, 7, 7, 7),
-//            mutableListOf(8, 8, 8, 8, 8, 8, 8, 8, 8),
-//            mutableListOf(9, 9, 9, 9, 9, 9, 9, 9, 9)
-//        )
-//    )
-//
-//    private val goodSquaresWrongSudoku = Board(
-//        mutableListOf(
-//            mutableListOf(1, 2, 3, 1, 2, 3, 1, 2, 3),
-//            mutableListOf(4, 5, 6, 4, 5, 6, 4, 5, 6),
-//            mutableListOf(7, 8, 9, 7, 8, 9, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 4, 4, 4, 1, 2, 3),
-//            mutableListOf(4, 5, 6, 5, 5, 5, 4, 5, 6),
-//            mutableListOf(7, 8, 9, 6, 6, 6, 7, 8, 9),
-//            mutableListOf(1, 2, 3, 1, 2, 3, 1, 2, 3),
-//            mutableListOf(4, 5, 6, 4, 5, 6, 4, 5, 6),
-//            mutableListOf(7, 8, 9, 7, 8, 9, 7, 8, 9)
-//        )
-//    )
-//
-//    private val properlySolvedSudoku = Board(
-//        mutableListOf(
-//            mutableListOf(4, 9, 8, 2, 6, 3, 1, 5, 7),
-//            mutableListOf(1, 3, 6, 5, 7, 8, 2, 9, 4),
-//            mutableListOf(5, 7, 2, 4, 9, 1, 6, 8, 3),
-//            mutableListOf(8, 1, 9, 3, 4, 2, 7, 6, 5),
-//            mutableListOf(6, 5, 3, 8, 1, 7, 9, 4, 2),
-//            mutableListOf(2, 4, 7, 6, 5, 9, 8, 3, 1),
-//            mutableListOf(7, 6, 1, 9, 3, 5, 4, 2, 8),
-//            mutableListOf(9, 8, 5, 1, 2, 4, 3, 7, 6),
-//            mutableListOf(3, 2, 4, 7, 8, 6, 5, 1, 9)
-//        )
-//    )
-//
-//    @Test
-//    fun `should return true with good input`() {
-//        assertTrue(sudokuVerifier.isSudokuSolved(properlySolvedSudoku))
-//    }
-//
-//    @Test
-//    fun `should return false with good rows`() {
-//        assertFalse(sudokuVerifier.isSudokuSolved(goodRowsWrongSudoku))
-//    }
-//
-//    @Test
-//    fun `should return false with good columns`() {
-//        assertFalse(sudokuVerifier.isSudokuSolved(goodColumnsWrongSudoku))
-//    }
-//
-//    @Test
-//    fun `should return false with good squares`() {
-//        assertFalse(sudokuVerifier.isSudokuSolved(goodSquaresWrongSudoku))
-//    }
-//}
+
+import com.example.sudokuapplication.model.Board
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class SudokuVerifierTest {
+
+    private val sudokuVerifier = SudokuVerifier()
+
+    private val goodRowsWrongSudoku = Board(
+        mutableListOf(
+            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        )
+    )
+
+    private val goodColumnsWrongSudoku = Board(
+        mutableListOf(
+            mutableListOf(1, 1, 1, 1, 1, 1, 1, 1, 1),
+            mutableListOf(2, 2, 2, 2, 2, 2, 2, 2, 2),
+            mutableListOf(3, 3, 3, 3, 3, 3, 3, 3, 3),
+            mutableListOf(4, 4, 4, 4, 4, 4, 4, 4, 4),
+            mutableListOf(5, 5, 5, 5, 5, 5, 5, 5, 5),
+            mutableListOf(6, 6, 6, 6, 6, 6, 6, 6, 6),
+            mutableListOf(7, 7, 7, 7, 7, 7, 7, 7, 7),
+            mutableListOf(8, 8, 8, 8, 8, 8, 8, 8, 8),
+            mutableListOf(9, 9, 9, 9, 9, 9, 9, 9, 9)
+        )
+    )
+
+    private val goodSquaresWrongSudoku = Board(
+        mutableListOf(
+            mutableListOf(1, 2, 3, 1, 2, 3, 1, 2, 3),
+            mutableListOf(4, 5, 6, 4, 5, 6, 4, 5, 6),
+            mutableListOf(7, 8, 9, 7, 8, 9, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 4, 4, 1, 2, 3),
+            mutableListOf(4, 5, 6, 5, 5, 5, 4, 5, 6),
+            mutableListOf(7, 8, 9, 6, 6, 6, 7, 8, 9),
+            mutableListOf(1, 2, 3, 1, 2, 3, 1, 2, 3),
+            mutableListOf(4, 5, 6, 4, 5, 6, 4, 5, 6),
+            mutableListOf(7, 8, 9, 7, 8, 9, 7, 8, 9)
+        )
+    )
+
+    private val properlySolvedSudoku = Board(
+        mutableListOf(
+            mutableListOf(4, 9, 8, 2, 6, 3, 1, 5, 7),
+            mutableListOf(1, 3, 6, 5, 7, 8, 2, 9, 4),
+            mutableListOf(5, 7, 2, 4, 9, 1, 6, 8, 3),
+            mutableListOf(8, 1, 9, 3, 4, 2, 7, 6, 5),
+            mutableListOf(6, 5, 3, 8, 1, 7, 9, 4, 2),
+            mutableListOf(2, 4, 7, 6, 5, 9, 8, 3, 1),
+            mutableListOf(7, 6, 1, 9, 3, 5, 4, 2, 8),
+            mutableListOf(9, 8, 5, 1, 2, 4, 3, 7, 6),
+            mutableListOf(3, 2, 4, 7, 8, 6, 5, 1, 9)
+        )
+    )
+
+    @Test
+    fun `should return true with good input`() {
+        assertTrue(sudokuVerifier.isSudokuSolved(properlySolvedSudoku.board.toMutableListOfCells()))
+    }
+
+    @Test
+    fun `should return false with good rows`() {
+        assertFalse(sudokuVerifier.isSudokuSolved(goodRowsWrongSudoku.board.toMutableListOfCells()))
+    }
+
+    @Test
+    fun `should return false with good columns`() {
+        assertFalse(sudokuVerifier.isSudokuSolved(goodColumnsWrongSudoku.board.toMutableListOfCells()))
+    }
+
+    @Test
+    fun `should return false with good squares`() {
+        assertFalse(sudokuVerifier.isSudokuSolved(goodSquaresWrongSudoku.board.toMutableListOfCells()))
+    }
+}
